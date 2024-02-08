4c335bdceab970ad0a672fb8729fdc3313b3d641
MikeYoulend <mikeyoulend@gmail.com>
2024-02-08 18:20:00 +0100

    Pitch, yaw & roll

--- Assets/Script/PlayerControl.cs
+++ Assets/Script/PlayerControl.cs
@@ -21,24 +21,38 @@ public class PlayerControl : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        ProcessTranslation();
+        ProcessRotation();
+
+        //float horizontalThrow = Input.GetAxis("Horizontal");
+        //float VerticalThrow = Input.GetAxis("Vertical");
+    }
+
+    void ProcessRotation()
+    {
+        float pitch = 0f;
+        float yaw = 0f;
+        float roll = 0f;
+
+        transform.localRotation = Quaternion.Euler(pitch, yaw, roll);
+    }
+
+    void ProcessTranslation()
+    {
         float xThrow = movement.ReadValue<Vector2>().x;
         float YThrow = movement.ReadValue<Vector2>().y;
 
         float xOffset = xThrow * Time.deltaTime * controlSpeed;
         float rawXPos = transform.localPosition.x + xOffset;
         float clampedXPos = Mathf.Clamp(rawXPos, -xRange, xRange);
-        
+
         float yOffset = YThrow * Time.deltaTime * controlSpeed;
         float rawYPos = transform.localPosition.y + yOffset;
         float clampedYPos = Mathf.Clamp(rawYPos, -yRange, yRange);
 
-        transform.localPosition = new Vector3 
-        (clampedXPos, 
-        clampedYPos, 
+        transform.localPosition = new Vector3
+        (clampedXPos,
+        clampedYPos,
         transform.localPosition.z);
-        
-       //float horizontalThrow = Input.GetAxis("Horizontal");
-       
-       //float VerticalThrow = Input.GetAxis("Vertical");
     }
 }
