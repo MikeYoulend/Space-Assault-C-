69783d2d2897794d90173d2b4f84bd2b358736b2
MikeYoulend <mikeyoulend@gmail.com>
2024-02-13 20:43:01 +0100

    Empty Parent

--- Assets/Script/Enemy.cs
+++ Assets/Script/Enemy.cs
@@ -5,13 +5,15 @@ using UnityEngine;
 public class Enemy : MonoBehaviour
 {
    [SerializeField] GameObject deathVFX;
+   [SerializeField] Transform parent; //transform perchè è l'unica cosa presente nell'Empty
 
     void OnParticleCollision(GameObject other) 
     {
         ///Instatiate clona un oggetto 
         ///transform.position perchè è la posizione di Enemy
         ///Quaternion.identity fa si che la copia rimanga cosi senza rotazioni o movivementi
-        Instantiate(deathVFX, transform.position, Quaternion.identity);
+        GameObject vfx = Instantiate(deathVFX, transform.position, Quaternion.identity);
+        vfx.transform.parent = parent; //daremo come parent Il parent sopra agli Enemy
         Destroy(gameObject); //distruggiamo il gameObject a cui diamo questo script  
     }
     
