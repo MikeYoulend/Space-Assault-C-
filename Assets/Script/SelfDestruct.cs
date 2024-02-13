27b640e5834be4587cc7362833efaabbf5e6ffea
MikeYoulend <mikeyoulend@gmail.com>
2024-02-13 20:48:08 +0100

    SelfDestruct Script

--- Assets/Script/SelfDestruct.cs
+++ Assets/Script/SelfDestruct.cs
@@ -4,15 +4,13 @@ using UnityEngine;
 
 public class SelfDestruct : MonoBehaviour
 {
+
+    [SerializeField] float TimeTillDestroy = 3f;
     // Start is called before the first frame update
     void Start()
     {
-        
+        Destroy(this.gameObject, TimeTillDestroy);
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+   
 }
