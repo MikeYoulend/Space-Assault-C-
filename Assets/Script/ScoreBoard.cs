94ae620186ea7c210bc81d9c676ecdb5bae9bc87
MikeYoulend <mikeyoulend@gmail.com>
2024-02-14 17:53:54 +0100

    score.ToString();

--- Assets/Script/ScoreBoard.cs
+++ Assets/Script/ScoreBoard.cs
@@ -1,14 +1,25 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
+using UnityEngine.SocialPlatforms.Impl; //stiamo usando TextMeshPro
 
 public class ScoreBoard : MonoBehaviour
 {
    int score;
+   TMP_Text scoreText; //Assegniamo TMP_Text;
+
+   void Start() 
+   {
+      scoreText = GetComponent<TMP_Text>();
+      scoreText.text = "Start";
+   }
+
+ 
 
    public void IncreaseScore(int amountToIncrease)
    {
-    score += amountToIncrease; //score = score + amountToIncrease;
-    Debug.Log(score);
+     score += amountToIncrease; //score = score + amountToIncrease;
+     scoreText.text = score.ToString(); //prendiamo lo start di sopra e gli assegniamo score.tostring() per farlo trasformare nel punteggio
    }
 }
